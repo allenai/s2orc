c7128d5cc80d2d8e0e2ef95ab052140165a54832
kyleclo <kyleclo@uw.edu>
2019-11-04 03:48:11 -0800

    init 2

---
+++ config.py
@@ -0,0 +1,7 @@
+S3_BUCKET_NAME = 'ai2-s2-gorc-release'
+S3_PREFIX_VERSION = '20190928/'
+
+S3_SETTINGS = {
+    'bucket': S3_BUCKET_NAME,
+    'dirname': S3_PREFIX_VERSION
+}
---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import setuptools
+
+setuptools.setup(
+    name='s2-gorc',
+    version='0.1',
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    tests_require=[
+    ],
+    zip_safe=False,
+    test_suite='py.test',
+    entry_points='',
+)
